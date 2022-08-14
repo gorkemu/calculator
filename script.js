7289c93f02b1b9d9e169f80806b0559d6972a653
gorkemu <gorkemunal88@gmail.com>
2022-08-15 01:42:11 +0300

    add more style

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 const numberButtons = document.querySelectorAll('.number-btn');
 const operatorButtons = document.querySelectorAll('.operator-btn');
-const clearButton = document.getElementById('clear-btn');
 const equalsButton = document.getElementById('equals-btn');
+const clearButton = document.getElementById('clear-btn');
 
 let lastOperator = '';
-let displayBottom = document.querySelector('#display-bottom');
 let displayTop = document.querySelector('#display-top');
 let displayMiddle = document.querySelector('#display-middle');
+let displayBottom = document.querySelector('#display-bottom');
 
 numberButtons.forEach(button =>
     button.addEventListener('click', () => {
         if (displayBottom.textContent === '0') {
             resetDisplay();
-        }    
+        }
         displayBottom.textContent += button.textContent;
     }
-));
+    ));
 
-clearButton.addEventListener('click', () => {
-    displayBottom.textContent = '0';
-    displayTop.textContent = '';
-    displayMiddle.textContent = '';
-});
-
-operatorButtons.forEach(button => 
+operatorButtons.forEach(button =>
     button.addEventListener('click', () => {
-        
+
         if (displayTop.textContent === '') {
             displayTop.textContent = displayBottom.textContent;
             displayBottom.textContent = '';
@@ -33,22 +27,51 @@ operatorButtons.forEach(button =>
             displayMiddle.textContent = button.textContent;
         }
         else {
+            displayMiddle.textContent = button.textContent;
             displayTop.textContent = operate(lastOperator, displayTop.textContent, displayBottom.textContent);
             displayBottom.textContent = '';
             lastOperator = button.textContent;
-            displayMiddle.textContent = button.textContent;
         }
     }));
-
-
+    
 equalsButton.addEventListener('click', () => {
     displayBottom.textContent = operate(lastOperator, displayTop.textContent, displayBottom.textContent);
+    if (displayBottom.textContent.length > 12) {
+        displayBottom.textContent = Number(displayBottom.textContent);
+    }
     displayTop.textContent = '';
     displayMiddle.textContent = '';
 });
 
+clearButton.addEventListener('click', () => {
+    resetDisplay();
+    displayBottom.textContent = '0';
+});
+
 function resetDisplay() {
-    displayBottom.textContent = ''
+    displayBottom.textContent = '';
+    displayTop.textContent = '';
+    displayMiddle.textContent = '';
+};
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '÷':
+            if (b === 0) {
+                displayMiddle.textContent = '';
+                alert("You can't divide by 0!");
+                return null;
+            }
+            else return divide(a, b);
+    }
 };
 
 function add(a, b) {
@@ -64,28 +87,9 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    if (b === 0) {
-        alert("You can't divide by 0!");
-    }
     return a / b;
-
 };
 
-function operate(operator, a, b) {
-    a = Number(a);
-    b = Number(b);
-    switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case 'x':
-            return multiply(a, b);
-        case '÷':
-            if (b === 0) return null;
-            else return divide(a, b);
-    }
-};
 
 
 
